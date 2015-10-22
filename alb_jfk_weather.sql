abe08b90a8e5130a5529bbc836d96e29c4c6c1cd
Nick Schiraldi <nschiraldi@gmail.com>
2015-10-22 11:15:17 -0400

    SQL Data and Code
    Added some text files for Albany METARS and JFK METARS (.csv files).
    
    weather_db.db is the database compiled from alb_jfk_weather.sql

---
+++ alb_jfk_weather.sql
@@ -0,0 +1,19 @@
+CREATE TABLE alb (
+    date TIMESTAMP PRIMARY KEY,
+    temperature FLOAT,
+    precipiation FLOAT,
+    max_t BOOL,
+    min_t BOOL,
+    wind_speed FLOAT,
+    wind_direction FLOAT
+);
+
+CREATE TABLE jfk (
+    date TIMESTAMP PRIMARY KEY,
+    temperature FLOAT,
+    precipiation FLOAT,
+    max_t BOOL,
+    min_t BOOL,
+    wind_speed FLOAT,
+    wind_direction FLOAT
+);
